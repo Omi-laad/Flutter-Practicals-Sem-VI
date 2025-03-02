36ef148b76434a1af52125c577f0397eab667e8c
omkarlaad <omkarlaad98@gmail.com>
2025-03-03 01:10:42 +0530

    exp5

--- exp5/lib/exp5.dart
+++ exp5/lib/exp5.dart
@@ -5,6 +5,32 @@ class Exp5 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Row Widget Example'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.red,
+            ),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.green,
+            ),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
